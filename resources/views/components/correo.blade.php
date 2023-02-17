6bd39e109edaee92312a44d74d1a32bd1574df71
Marc García-Cuevas de Paz <marcg-c20@hotmail.es>
2023-02-17 18:25:30 +0100

    base

--- resources/views/components/correo.blade.php
+++ resources/views/components/correo.blade.php
@@ -1,16 +1,13 @@
+<div class="caja">
+    <div class="formmail">
+        <form action="" method="post">
+            <input type="text" id="destinatario" name="Destinatario" placeholder="Destinatario">
 
+            <input type="text" id="asunto" name="Asunto" placeholder="Asunto">
 
-
-
-
-
-
-<form action="" method="post">
-
-    <input type="text" placeholder="Destinatario">
-
-    <input type="text" placeholder="Asunto">
-
-    <input type="text" placeholder="Asunto">
-
-</form>
+            <textarea name="" id="mensaje" name="Mensaje" placeholder="Mensaje"></textarea>
+            
+            <input type="submit" value="ENVIAR CORREO" id="enviar">
+        </form>
+    </div>
+</div>

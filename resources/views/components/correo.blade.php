5499ae8223986b19b8794e7b3852305d78f95726
Marc García-Cuevas de Paz <marcg-c20@hotmail.es>
2023-02-23 15:27:01 +0100

    arreglo

--- resources/views/components/correo.blade.php
+++ resources/views/components/correo.blade.php
@@ -1,13 +1,17 @@
 <div class="caja">
     <div class="formmail">
         <form action="" method="post">
+            <ion-icon name="mail-outline"></ion-icon>
+            <h1>ENVIAR MAIL</h1>
             <input type="text" id="destinatario" name="Destinatario" placeholder="Destinatario">
 
             <input type="text" id="asunto" name="Asunto" placeholder="Asunto">
 
             <textarea name="" id="mensaje" name="Mensaje" placeholder="Mensaje"></textarea>
-            
-            <input type="submit" value="ENVIAR CORREO" id="enviar">
+
+            <input type="submit" value="ENVIAR CORREO" id="enviar" class="enviar">
         </form>
     </div>
 </div>
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
